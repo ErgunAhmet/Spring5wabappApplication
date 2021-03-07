a7c30de666f1079ae4ce72809dddf26ec1962911
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-03-07 23:45:14 +0100

    checking if i can change my code on github

--- src/main/java/guru/springframework/spring5wabapp/Spring5wabappApplication.java
+++ src/main/java/guru/springframework/spring5wabapp/Spring5wabappApplication.java
@@ -8,6 +8,7 @@ public class Spring5wabappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring5wabappApplication.class, args);
+        System.out.println("hello world");
     }
 
 }
